803a111b4e603a9414cb0513af37e3c61420f2ea
David Thompson <106985370+quynhnhubks@users.noreply.github.com>
2022-12-14 11:37:42 +0000

    JS number exercise

---
+++ js-course/script.js
@@ -0,0 +1,25 @@
+// Number 1 exercise
+
+let age = 40;
+
+console.log(age);
+
+// Number 2 exercise
+
+let numberOfRabbits = 2;
+numberOfRabbits *= 3;
+
+console.log(numberOfRabbits);
+
+// Number 3 exercise
+
+let bodmassExample = 3;
+bodmassExample += 5 / 6;
+
+console.log(bodmassExample);
+
+// number 4 exercise
+
+
+
+console.log( Math.floor(Math.random() * 7) + 1 );
